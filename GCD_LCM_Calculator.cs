6b7d115899326e78eed20653de4fc7ac7a7eed3a
Kooreis <kooreis.official@gmail.com>
2023-04-10 09:20:54 -0500

    Initial setup of the program. It prompts the user to input two numbers.

--- GCD_LCM_Calculator.cs
+++ GCD_LCM_Calculator.cs
@@ -1,32 +1,13 @@
-```CSharp
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int num1, num2, gcd, lcm;
+        int num1, num2;
         Console.Write("Enter the First Number : ");
         num1 = int.Parse(Console.ReadLine());
         Console.Write("Enter the Second Number : ");
         num2 = int.Parse(Console.ReadLine());
-        gcd = GCD(num1, num2);
-        lcm = LCM(num1, num2);
-        Console.WriteLine("GCD of {0} and {1} is {2}", num1, num2, gcd);
-        Console.WriteLine("LCM of {0} and {1} is {2}", num1, num2, lcm);
-        Console.ReadLine();
-    }
-
-    static int GCD(int a, int b)
-    {
-        if (b == 0)
-            return a;
-        return GCD(b, a % b);
-    }
-
-    static int LCM(int a, int b)
-    {
-        return (a * b) / GCD(a, b);
     }
 }
-```
