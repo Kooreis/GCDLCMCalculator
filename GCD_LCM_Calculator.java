7970b80fd3c960b9888bdbe52b3c5046a0af603f
Kooreis <kooreis.official@gmail.com>
2023-04-11 09:20:54 -0500

    In the second update, user input is requested for two numbers, which are then stored in the variables 'num1' and 'num2'.

--- GCD_LCM_Calculator.java
+++ GCD_LCM_Calculator.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
     }
 }
